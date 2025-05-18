b817e9991eead6ffd8eb88e8e4b62bdd7c7da3ed
dzemre <luthiena-dzemre.akcinara@edu.rtu.lv>
2025-05-19 01:20:51 +0300

    Update appp.py

--- appp.py
+++ appp.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_tempalte, request
 import requests
 import matplotlib.pyplot as plt
-import dateitme
+import datetime
 import os
+
+app = Flask(__name__, static_folder="currency_chart/static")
+
+API_KEY = 'fca_live_xa0BIncYUXOowI2zzWTs7xjCvAEB7QGdlUi7Of2o'
+BASE_URL = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
