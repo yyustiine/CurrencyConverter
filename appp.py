ba01012f8bd4be5411a2a7013ad5084df85e1f3f
dzemre <luthiena-dzemre.akcinara@edu.rtu.lv>
2025-05-19 18:07:04 +0300

    Update appp.py

--- appp.py
+++ appp.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_tempalte, request
+from flask import Flask, render_template, request
 import requests
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import datetime
 import os
