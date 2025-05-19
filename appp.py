81bdd8af707e322b7fdcca66ba329462dbbfe42c
dzemre <luthiena-dzemre.akcinara@edu.rtu.lv>
2025-05-19 18:56:28 +0300

    Update appp.py

--- appp.py
+++ appp.py
@@ -10,12 +10,16 @@ app = Flask(__name__, static_folder="currency_chart/static")
 
 API_KEY = 'fca_live_xa0BIncYUXOowI2zzWTs7xjCvAEB7QGdlUi7Of2o'
 BASE_URL = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
-HIST_URL = "https://api.freecurrencyapi.com/v1/historic" 
+HISTORICAL_URL = "https://api.freecurrencyapi.com/v1/historical"
 CURRENCIES = ["USD", "CAD", "EUR", "AUD", "CNY", "GBP", "CHF", "JPY", "HKD", "SGD"]
 
-def draw_chart(currency, rates, time_labels):
-    if os.path.exists("currency_chart/static/chart.png"):
-        os.remove("currency_chart/static/chart.png")
+def draw_chart(currency_code, rates, dates):
+    chart_dir = 'currency_chart_fixed/static'
+    chart_path = os.path.join(chart_dir, 'chart.png')
+
+    os.makedirs(chart_dir, exist_ok=True)
+    if os.path.exists(chart_path):
+        os.remove(chart_path)
         
     plt.figure(figsize=(10, 5))
     plt.plot(time_labels, rates, color="green")
